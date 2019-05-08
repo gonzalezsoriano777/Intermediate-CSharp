9d803582e3c7819a63f256c0129d05e14b149333
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-05-08 10:41:33 -0500

    prac with indexers

---
+++ Intermediate-CSharp/Indexers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intermediate_CSharp
+{
+    public class Indexers
+    {
+        private readonly Dictionary<string, string> _dictionary;
+
+        public Indexers()
+        {
+            _dictionary = new Dictionary<string, string>();
+        }
+
+
+    }
+}
