2b64bb36ee44e2337c0990877289cc4556734519
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-05-09 11:20:57 -0500

    created constructors

--- Intermediate-CSharp/Vehicle.cs
+++ Intermediate-CSharp/Vehicle.cs
@@ -10,5 +10,10 @@ namespace Intermediate_CSharp
     public class Vehicle
     {
         private readonly string _registrationNumber;
+
+        public Vehicle(string registrationNumber)
+        {
+            _registrationNumber = registrationNumber;
+        }
     }
 }
