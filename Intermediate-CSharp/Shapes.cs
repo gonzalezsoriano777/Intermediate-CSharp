4fc9d15bcda885b143308e054204c558e72f5cbb
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-05-09 18:57:47 -0500

    test

--- Intermediate-CSharp/Shapes.cs
+++ Intermediate-CSharp/Shapes.cs
@@ -8,6 +8,15 @@ namespace Intermediate_CSharp
 {
     public class Shapes
     {
+        // created fields that will be used for the sized of the shapes
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Position Position { get; set; }
+
+        public virtual void Draw()
+        {
+
+        }
 
     }
 }
