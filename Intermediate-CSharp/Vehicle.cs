ba9db88bd88a9e029f7380a5ebc98c3c0a475b11
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-05-09 11:18:50 -0500

    test

---
+++ Intermediate-CSharp/Vehicle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intermediate_CSharp
+{
+    // focusing on using constructor that use private modifiers and how to include inheritance using a property that is private
+    public class Vehicle
+    {
+        private readonly string _registrationNumber;
+    }
+}
