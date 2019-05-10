002ccfb4936490a0c2e1d54368c9945ea1a2e101
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-05-09 19:14:43 -0500

    test

--- Intermediate-CSharp/Shapes.cs
+++ Intermediate-CSharp/Shapes.cs
@@ -6,15 +6,17 @@ using System.Threading.Tasks;
 
 namespace Intermediate_CSharp
 {
-
+    // this is polymorphism.
     public class Circle : Shapes
     {
         public override void Draw()
         {
+            Console.WriteLine();
             Console.WriteLine("This shape is a circle");
         }
     }
 
+    // we are able to create new classes that have no effect or impact
     public class Rectangle : Shapes
     {
         public override void Draw()
@@ -23,6 +25,7 @@ namespace Intermediate_CSharp
         }
     }
 
+    // meaning that each class that revolves around different shapes won't derive from other shapes
     public class Triangle : Shapes
     {
         public override void Draw()
@@ -36,7 +39,6 @@ namespace Intermediate_CSharp
         // created fields that will be used for the sized of the shapes
         public int Width { get; set; }
         public int Height { get; set; }
-        public Position Position { get; set; }
 
         public virtual void Draw()
         {
