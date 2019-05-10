896f75baf3cf7fd1d6136e58d2d7497d640c6886
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-05-10 13:54:32 -0500

    construct. set for file logger.

---
+++ Intermediate-CSharp/FileLogger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intermediate_CSharp
+{
+    public class FileLogger : ILogger
+    {
+        private readonly string _path;
+
+        public FileLogger(string path)
+        {
+            _path = path;      
+        }
+
+        public void LogError(string message)
+        {
+            using ()
+            {
+
+            } 
+            
+        }
+
+        public void LogInfo(string message)
+        {
+            using ()
+            {
+
+            }
+        }
+    }
+}
