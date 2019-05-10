da85d6bca3ffa143f8fccebf51bad27bdc6cb931
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-05-09 19:00:02 -0500

    tets

---
+++ Intermediate-CSharp/ShapeTypes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intermediate_CSharp
+{
+    // predefined values that will be used for the purpose of the application
+    public enum ShapeTypes
+    {
+        Circle,
+        Rectangle,
+        Triangle
+
+    }
+}
