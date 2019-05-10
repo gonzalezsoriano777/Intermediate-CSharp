f25123eab1cdbe9ab55e3f9a05acb35e75eee042
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-05-10 13:17:05 -0500

    work

---
+++ Intermediate-CSharp/DbMigratorv2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intermediate_CSharp
+{
+    public class DbMigratorv2
+    {
+        private readonly ILogger _logger;
+
+        public DbMigratorv2(ILogger logger)
+        {
+            _logger = logger;
+        }
+    }
+}
