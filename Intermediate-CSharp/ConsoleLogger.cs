9064b93e09ac33298479367c1985208cfa64df64
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-05-10 13:44:49 -0500

    test

--- Intermediate-CSharp/ConsoleLogger.cs
+++ Intermediate-CSharp/ConsoleLogger.cs
@@ -6,14 +6,18 @@ using System.Threading.Tasks;
 
 namespace Intermediate_CSharp
 {
+    // this will be response toward the behavior once called to the console
     public class ConsoleLogger : ILogger
     {
+
+        // for both interfaces once the 'info' is called to the console
         public void LogError(string message)
         {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine(message);
         }
 
+        // the color of the text if it's info will be green but if migration is failed then red
         public void LogInfo(string message)
         {
             Console.ForegroundColor = ConsoleColor.Green;
