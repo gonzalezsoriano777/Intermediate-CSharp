f700118cad735ce6e0571465de3df28fd749ab42
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-05-10 13:32:40 -0500

    y

--- Intermediate-CSharp/DbMigratorv2.cs
+++ Intermediate-CSharp/DbMigratorv2.cs
@@ -11,6 +11,7 @@ namespace Intermediate_CSharp
         // created a private field that will be used for the migration of the database
         private readonly ILogger _logger;
 
+
         public DbMigratorv2(ILogger logger)
         {
             _logger = logger;
@@ -18,12 +19,12 @@ namespace Intermediate_CSharp
 
         public void Migrate()
         {
-            // the purpose of this practice is that we work w/ the applications behavior and be able change the way it processes with lang. constructors
-            Console.WriteLine("Migration started at {0}", DateTime.Now);
+            // the purpose of this practice is that we work w/ the applications behavior and be able change the way it processes
+            _logger.LogInfo("Migration started at {0}" + DateTime.Now);
 
             // Overall details of the migration of the db
 
-            Console.WriteLine("Migration finished at {0}", DateTime.Now);
+            _logger.LogInfo("Migration finished at {0}" + DateTime.Now);
         }
 
     }
