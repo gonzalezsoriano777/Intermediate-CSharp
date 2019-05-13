afeec07e16d4b05c6b160fedb41b6f3592bbf041
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-05-13 18:24:47 -0500

    test

---
+++ Intermediate-CSharp/OrderProcessor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intermediate_CSharp
+{
+    public class OrderProcessor
+    {
+        private readonly ShippingCalculator _shippingCalculator;    
+
+        public OrderProcessor(ShippingCalculator shippingCalculator)
+        {
+            _shippingCalculator = shippingCalculator;
+        }
+
+        public void Order(Order order){
+            if (order.isShipping)
+            {
+                throw InvalidOperationException("Order is already processing");
+            }
+    }
+}
---
+++ Intermediate-CSharp/ShippingCalculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intermediate_CSharp
+{
+    public class ShippingCalculator
+    {
+        public void CalculateShipping(Order order)
+        {
+            // Once totalPrice for product is shown, shipping price will be declared if price is below $30
+            if (order.TotalPrice > 30f)
+                return order.TotalPrice * 0.1f;
+
+                // but if price is above 30 then it's free shipping
+                return 0;
+        }
+    }
+}
