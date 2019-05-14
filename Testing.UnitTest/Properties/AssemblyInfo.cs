5708d12df5927b260196f94ad31a69b58ece620c
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-05-13 19:17:59 -0500

    test

---
+++ Testing.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Testing.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Testing.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2eba0c94-e13b-4cc1-975c-e63492c54c29")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Testing.UnitTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing.UnitTest
+{
+    [TestClass]
+    public class OrderProcessorTest
+    {
+
+        // METHODNAME_CONDITION_EXPECTATION
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
