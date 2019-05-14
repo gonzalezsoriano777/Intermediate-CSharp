9caef0a4b2b62440ef8c73cf905dbad3d27c2170
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-05-13 19:53:14 -0500

    f

--- Intermediate-CSharp/Order.cs
+++ Intermediate-CSharp/Order.cs
@@ -8,5 +8,11 @@ namespace Intermediate_CSharp
 {
     public class Order
     {
+        public int ID { get; set; }
+        public DateTime DatePlaced { get; set; }
+        public Shipment shipment { get; set; }
+        public float TotalPrice { get; set; }
+        
+        
     }
 }
